37568f90dd967b4e652bb3dea5795f5bf5aa1128
Muthoni(sonnieperis) <sonnieperis17@gmail.com>
2025-11-20 17:12:56 +0300

    Implemented basic routing using the Pages Router in Next.js

--- alx-project-2/components/layout/Header.tsx
+++ alx-project-2/components/layout/Header.tsx
@@ -1,19 +1,13 @@
 import Link from "next/link";
-import React from "react";
 
-const Header: React.FC = () => {
+export default function Header() {
   return (
-    <header className="w-full bg-blue-600 text-white py-4 shadow">
-      <div className="max-w-6xl mx-auto px-4 flex items-center justify-between">
-        <div className="text-xl font-bold">ALX Project 2</div>
-        <nav className="flex gap-4">
-          <Link href="/"><a className="hover:underline">Home</a></Link>
-          <Link href="/posts"><a className="hover:underline">Posts</a></Link>
-          <Link href="/users"><a className="hover:underline">Users</a></Link>
-        </nav>
-      </div>
+    <header className="w-full bg-blue-700 text-white p-4">
+      <nav className="flex space-x-6">
+        <Link href="/">Home</Link>
+        <Link href="/home">Home Page</Link>
+        <Link href="/about">About</Link>
+      </nav>
     </header>
   );
-};
-
-export default Header;
+}
